ff291af80648913dab010fc06eacebc073a855f3
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-18 21:59:32 -0800

    converting points to python

---
+++ points.py
@@ -0,0 +1,9 @@
+points = int(input("How many points do you have? "))
+
+
+if points < 2:
+    print("You have less point than me")
+elif points > 2:
+    print("You have more points than me")
+else:
+    print(" You have the same points as me")

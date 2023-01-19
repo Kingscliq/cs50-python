9c9a9f6d37dc79d0aab0f41fcf5ec0e10e8f66fa
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-18 19:22:44 -0800

    Try Catch to manage exceptions

--- calculator.py
+++ calculator.py
@@ -1,5 +1,19 @@
-x = int(input("Enter x: "))
-y = int(input("Enter y: "))
+# x = int(input("Enter x: "))
+# y = int(input("Enter y: "))
+
+# sum = x + y
+
+# print(f"Sum is {sum}")
+
+try:
+    x = int(input("Enter x: "))
+except ValueError:
+    print("This is not a number")
+
+try:
+    y = int(input("Enter y: "))
+except ValueError:
+    print("This is not a number")
 
 sum = x + y
 

25e65032496f232704127deb8d28cc2e93717344
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-18 19:16:24 -0800

    Update: building a mini calculator in python

---
+++ calculator.py
@@ -0,0 +1,6 @@
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+sum = x + y
+
+print(f"Sum is {sum}")
--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-answer = "Kingsley"
+answer = input("What is your name? ")
 
-print(f"Hello, {answer}")
+print("Hello, " + answer)

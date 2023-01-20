258df7b02c0eb1102ef1d889d787b7343dffc9de
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-19 22:30:57 -0800

    Add: printing mario bricks

--- mario.py
+++ mario.py
@@ -1,10 +1,19 @@
 
-while True:
-    try:
-        height = int(input("Height: "))
-        if (height > 0):
-            break
-        for i in range(height):
-            print("#")
-    except ValueError:
-        print("This is not an integer!, Please enter an interger")
+# while True:
+#     try:
+#         height = int(input("Height: "))
+#         if (height > 0):
+#             break
+#         for i in range(height):
+#             print("#")
+#     except ValueError:
+#         print("This is not an integer!, Please enter an interger")
+
+
+# Printing on the same line
+# print("?" * 5)
+
+for i in range(3):
+    for j in range(3):
+        print("#")
+    print("")

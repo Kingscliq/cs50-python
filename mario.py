0a5119afd58aea617cab88531b87bcedd0a9674e
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-19 22:14:57 -0800

    Add: Catching exceptions for input types

--- mario.py
+++ mario.py
@@ -1,4 +1,10 @@
-height = int(input("Height: "))
 
-for i in range(height):
-    print("#")
+while True:
+    try:
+        height = int(input("Height: "))
+        if (height > 0):
+            break
+        for i in range(height):
+            print("#")
+    except ValueError:
+        print("This is not an integer!, Please enter an interger")

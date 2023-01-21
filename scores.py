de5a30b83800ac4446abbe5e98bf156e9ad3cb7f
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-20 23:28:32 -0800

    add: calculating average by getting input from user

--- scores.py
+++ scores.py
@@ -1,4 +1,13 @@
-scores = [73, 45, 32]
+# scores = [73, 45, 32]
+
+
+# DYANMICALLY
+
+scores = []
+for i in range(3):
+    score = int(input("Enter Score: "))
+    scores.append(score)
+
 
 average = sum(scores) / len(scores)
 
